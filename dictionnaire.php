bb4afeeaa2a795506503f365de8c5c679d4b0e27
gaksss <clement.carlier2@gmail.com>
2024-12-04 11:39:09 +0100

    Dictionnaire fini

--- dictionnaire.php
+++ dictionnaire.php
@@ -8,11 +8,35 @@ $dico = explode("\n", $string);
 // echo count($dico);
 
 // Nombre de mots a 15 lettres
-$count = 0;
-foreach ($dico as $key => $mot) {
-    if (strlen($mot) == 16) {
-        $count++;
+// $count = 0;
+// foreach ($dico as $key => $mot) {
+//     if (strlen($mot) == 16) {
+//         $count++;
         
-    };
+//     };
+// }
+// echo $count;
+
+
+// Nombre de mots avec w dedans 
+
+// $count = 0;
+// foreach ($dico as $key => $mot) {
+//     if (str_contains($mot, 'w')) {
+//         $count++;
+        
+//     };
+// }
+// echo $count;
+
+// Mots finissant par Q
+
+$count = 0;
+
+foreach ($dico as $mot) {
+    $mot = trim($mot);
+    if (str_ends_with($mot, 'q')) {
+        $count++;
+    }
 }
 echo $count;
